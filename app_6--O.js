c73f346f03eb6f12b610a3e6fdb4faa47ed9ad57
yadavnidhimvlu-max <yadavnidhimvlu@gmail.com>
2026-09-22 09:04:26 +0530

    Add files via upload

---
+++ app_6--O.js
@@ -0,0 +1,30 @@
+﻿if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js')
+        .then(() => console.log('Service Worker Registered'));
+}
+
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+displayCart();
+
+function addToCart(product) {
+    cart.push(product);
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    displayCart();
+}
+
+function displayCart() {
+    let cartList = document.getElementById('cartList');
+
+    cartList.innerHTML = "";
+
+    cart.forEach(item => {
+        let li = document.createElement('li');
+
+        li.textContent = item;
+
+        cartList.appendChild(li);
+    });
+}
